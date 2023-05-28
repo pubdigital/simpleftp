046092d0788e69ff631997a0e2bc3e7104a92656
Bruno Lategano <57153299+BrunoLategano@users.noreply.github.com>
2023-05-28 00:08:41 -0300

    Update myftp_skel.c
    Chequeo de argumentos en el lado del cliente y del servidor.

--- myftp_skel.c
+++ myftp_skel.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
 #include <err.h>
-
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
 #define BUFSIZE 512
+
+#define FINPUERTO 65535
 
 /**
  * function: receive and analize the answer from the server
@@ -23,14 +23,10 @@
 bool recv_msg(int sd, int code, char *text) {
     char buffer[BUFSIZE], message[BUFSIZE];
     int recv_s, recv_code;
-
     // receive the answer
-
-
     // error checking
     if (recv_s < 0) warn("error receiving data");
     if (recv_s == 0) errx(1, "connection closed by host");
-
     // parsing the code and message receive from the answer
     sscanf(buffer, "%d %[^\r\n]\r\n", &recv_code, message);
     printf("%d %s\n", recv_code, message);
@@ -39,7 +35,6 @@ bool recv_msg(int sd, int code, char *text) {
     // boolean test for the code
     return (code == recv_code) ? true : false;
 }
-
 /**
  * function: send command formated to the server
  * sd: socket descriptor
@@ -48,17 +43,13 @@ bool recv_msg(int sd, int code, char *text) {
  **/
 void send_msg(int sd, char *operation, char *param) {
     char buffer[BUFSIZE] = "";
-
     // command formating
     if (param != NULL)
         sprintf(buffer, "%s %s\r\n", operation, param);
     else
         sprintf(buffer, "%s\r\n", operation);
-
     // send command and check for errors
-
 }
-
 /**
  * function: simple input from keyboard
  * return: input without ENTER key
@@ -70,7 +61,6 @@ char * read_input() {
     }
     return NULL;
 }
-
 /**
  * function: login process from the client side
  * sd: socket descriptor
@@ -78,33 +68,22 @@ char * read_input() {
 void authenticate(int sd) {
     char *input, desc[100];
     int code;
-
     // ask for user
     printf("username: ");
     input = read_input();
-
     // send the command to the server
     
     // relese memory
     free(input);
-
     // wait to receive password requirement and check for errors
-
-
     // ask for password
     printf("passwd: ");
     input = read_input();
-
     // send the command to the server
-
-
     // release memory
     free(input);
-
     // wait for answer and process it and check for errors
-
 }
-
 /**
  * function: operation get
  * sd: socket descriptor
@@ -114,47 +93,32 @@ void get(int sd, char *file_name) {
     char desc[BUFSIZE], buffer[BUFSIZE];
     int f_size, recv_s, r_size = BUFSIZE;
     FILE *file;
-
     // send the RETR command to the server
-
     // check for the response
-
     // parsing the file size from the answer received
     // "File %s size %ld bytes"
     sscanf(buffer, "File %*s size %d bytes", &f_size);
-
     // open the file to write
     file = fopen(file_name, "w");
-
     //receive the file
-
-
-
     // close the file
     fclose(file);
-
     // receive the OK from the server
-
 }
-
 /**
  * function: operation quit
  * sd: socket descriptor
  **/
 void quit(int sd) {
     // send command QUIT to the client
-
     // receive the answer from the server
-
 }
-
 /**
  * function: make all operations (get|quit)
  * sd: socket descriptor
  **/
 void operate(int sd) {
     char *input, *op, *param;
-
     while (true) {
         printf("Operation: ");
         input = read_input();
@@ -178,7 +142,6 @@ void operate(int sd) {
     }
     free(input);
 }
-
 /**
  * Run with
  *         ./myftp <SERVER_IP> <SERVER_PORT>
@@ -189,15 +152,37 @@ int main (int argc, char *argv[]) {
 
     // arguments checking
 
+	int i, cont_ip, cont_puerto;
+
+	if(argc!=3){	//se comprueba si se recibe la cantidad de argumentos indicados
+	   printf("Ingrese la direccion IP y puerto del servidor\n");
+	   exit(1);
+	}
+	else{
+	   for(i = 0; i < strlen(argv[1]);i++){	//se toma el primer argumento recibido y analiza caracter por caracter si es un numero o punto
+	   	cont_ip = argv[1][i]-48;	//resta 48 para convertir el caracter en numero
+	   	if((cont_ip < 0 || cont_ip >9) && (cont_ip != -2)){	//-2 es el resultado de restar 48 al codigo ascii del punto (.)
+	   	   printf("IP ingresada no valida\n");
+            	   exit(1);
+               	}
+	   }
+	}
+	   for(i = 0; i < strlen(argv[2]);i++){ //se toma el segundo argumento y analiza caracter por caracter si es un numero o no
+	       cont_puerto = argv[2][i]-48;
+	       if(cont_puerto < 0 || cont_puerto >9){	
+	          printf("Puerto ingresado no valido\n");
+            	  exit(1);
+               }
+	   }
+	   if ((atoi(argv[2])<0)||(atoi(argv[2])>FINPUERTO)){	//se analiza si el puerto esta dentro del rango adecuado (0-65535)
+		printf("Ingrese un puerto valido\n");
+		exit(1);
+	   }
+
     // create socket and check for errors
-    
-    // set socket data    
 
-    // connect and check for errors
-
-    // if receive hello proceed with authenticate and operate if not warning
-
-    // close socket
-
-    return 0;
-}
+    // set socket data 
+	
+	
+	
+	
