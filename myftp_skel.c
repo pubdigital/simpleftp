cfee66540a22f849092d2db5199a6792e395bd4f
Bruno Lategano <57153299+BrunoLategano@users.noreply.github.com>
2023-05-30 23:37:14 -0300

    Creacion y seteo de socket del cliente.

--- myftp_skel.c
+++ myftp_skel.c
@@ -28,21 +28,21 @@ bool recv_msg(int sd, int code, char *text) {
     // receive the answer
     // error checking
     
-	if (recv_s < 0) warn("error receiving data");
+    if (recv_s < 0) warn("error receiving data");
     if (recv_s == 0) errx(1, "connection closed by host");
     
-	// parsing the code and message receive from the answer
+    // parsing the code and message receive from the answer
     
-	sscanf(buffer, "%d %[^\r\n]\r\n", &recv_code, message);
+    sscanf(buffer, "%d %[^\r\n]\r\n", &recv_code, message);
     printf("%d %s\n", recv_code, message);
     
-	// optional copy of parameters
+    // optional copy of parameters
     
-	if(text) strcpy(text, message);
+    if(text) strcpy(text, message);
     
-	// boolean test for the code
+    // boolean test for the code
     
-	return (code == recv_code) ? true : false;
+    return (code == recv_code) ? true : false;
 }
 
 /**
@@ -200,36 +200,49 @@ int main (int argc, char *argv[]) {
 
     // arguments checking
 
-	int i, cont_ip, cont_puerto;
+    int i, cont_ip, cont_puerto;
 
-	if(argc!=3){	//se comprueba si se recibe la cantidad de argumentos indicados
-	   printf("Ingrese la direccion IP y puerto del servidor\n");
-	   exit(1);
-	}
-	else{
-	   for(i = 0; i < strlen(argv[1]);i++){	//se toma el primer argumento recibido y analiza caracter por caracter si es un numero o punto
-	   	cont_ip = argv[1][i]-48;	//resta 48 para convertir el caracter en numero
-	   	if((cont_ip < 0 || cont_ip >9) && (cont_ip != -2)){	//-2 es el resultado de restar 48 al codigo ascii del punto (.)
-	   	   printf("IP ingresada no valida\n");
-            	   exit(1);
-               	}
-	   }
-	}
-	   for(i = 0; i < strlen(argv[2]);i++){ //se toma el segundo argumento y analiza caracter por caracter si es un numero o no
-	       cont_puerto = argv[2][i]-48;
-	       if(cont_puerto < 0 || cont_puerto >9){	
-	          printf("Puerto ingresado no valido\n");
-            	  exit(1);
+    if(argc!=3){    //se comprueba si se recibe la cantidad de argumentos indicados
+       printf("Ingrese la direccion IP y puerto del servidor\n");
+       exit(1);
+    }
+    else{
+       for(i = 0; i < strlen(argv[1]);i++){ //se toma el primer argumento recibido y analiza caracter por caracter si es un numero o punto
+        cont_ip = argv[1][i]-48;    //resta 48 para convertir el caracter en numero
+        if((cont_ip < 0 || cont_ip >9) && (cont_ip != -2)){ //-2 es el resultado de restar 48 al codigo ascii del punto (.)
+           printf("IP ingresada no valida\n");
+                   exit(1);
+                }
+       }
+    }
+       for(i = 0; i < strlen(argv[2]);i++){ //se toma el segundo argumento y analiza caracter por caracter si es un numero o no
+           cont_puerto = argv[2][i]-48;
+           if(cont_puerto < 0 || cont_puerto >9){   
+              printf("Puerto ingresado no valido\n");
+                  exit(1);
                }
-	   }
-	   if ((atoi(argv[2])<0)||(atoi(argv[2])>FINPUERTO)){	//se analiza si el puerto esta dentro del rango adecuado (0-65535)
-		printf("Ingrese un puerto valido\n");
-		exit(1);
-	   }
+       }
+       if ((atoi(argv[2])<0)||(atoi(argv[2])>FINPUERTO)){   //se analiza si el puerto esta dentro del rango adecuado (0-65535)
+        printf("Ingrese un puerto valido\n");
+        exit(1);
+       }
 
     // create socket and check for errors
 
+    sd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sd < 0) {
+           printf("Error en la creacion del socket\n");
+           exit(1);
+    }
+
     // set socket data    
+
+
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = inet_addr(argv[1]);
+    addr.sin_port = htons(atoi(argv[2]));
+
     // connect and check for errors
     // if receive hello proceed with authenticate and operate if not warning
     // close socket
